583655ac183d0f0eebf8d8a50f1b6d1998471c64
Osam Sakhi <msakhi21@gmail.com>
2014-10-21 13:01:51 -0400

    Colors. Still need to resize carousel images

--- _includes/deployment/deploy.sh
+++ _includes/deployment/deploy.sh
@@ -1,12 +1,19 @@
+cd ../../
+echo "Building
 
-echo "starting"
+"
 
+jekyll build
+
+echo "
+
+Deploying
+
+"
+scp -r _site/* mosakhi@sakhi.es:/home/mosakhi/public_html/osama/
+cd _includes/deployment/
 
 # rsync -v -e ssh --rsync-path=/usr/bin/rsync ../../_site/* mosakhi@sakhi.es:/home/mosakhi/public_html/osama/jekyll/
-
-scp -r ../../_site/* mosakhi@sakhi.es:/home/mosakhi/public_html/osama/
-
-# cd _includes
 # /usr/bin/rsync -avzhe ssh _site/ mosakhi@sakhi.es:/home/mosakhi/public_html/osama/jekyll/
 # jekyll && rsync -avz --delete _site/ ieee@ieee.gatech.edu:/httpsdocs/test/
 # rsync -v -e ssh _site/* ieee@ieee.gatech.edu:/httpsdocs/test/
